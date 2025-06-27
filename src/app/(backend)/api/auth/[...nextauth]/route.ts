7d068404d37b08e02f0da4a14c75437e052c1ea9
Dodong2 <carlseighartaliode@gmail.com>
2025-06-27 15:56:41 +0800

    testing deployment

--- src/app/(backend)/api/auth/[...nextauth]/route.ts
+++ src/app/(backend)/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,8 @@
-import NextAuth from "next-auth";
+import NextAuth, { AuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prisma } from "@/lib/db";
-import { AuthOptions } from "next-auth";
+
 
 type ExtendedProfile = {
   picture?: string;
