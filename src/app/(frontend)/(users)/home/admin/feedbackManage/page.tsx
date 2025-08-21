ce1a080fe316f54010f117ab09d335da28aab3a6
Dodong2 <carlseighartaliode@gmail.com>
2025-08-21 21:38:18 +0800

    ver-5.9 added localStorage for feedback read admin

--- src/app/(frontend)/(users)/home/admin/feedbackManage/page.tsx
+++ src/app/(frontend)/(users)/home/admin/feedbackManage/page.tsx
@@ -1,16 +1,15 @@
 'use client'
-import { useState } from "react"
 import { useFeedback } from "@/hooks/feedback/useFeedback"
+/* hooks */
+import { useLocalStorageAdmin } from "@/hooks/feedback/useLocalStorageAdmin";
 /* icon */
 import { FiPlus } from 'react-icons/fi';
 import { IoClose } from "react-icons/io5";
 
 export default function FeedbackManagePage() {
     const { data: feedbacksData, isLoading } = useFeedback().useGetFeedbacks()
-    const [openId, setOpenId] = useState<string | null>(null)
-
- 
-
+    const { openId, readIds, handleToggle } = useLocalStorageAdmin()
+    
     return (
         <div className="p-4 space-x-4">
             <h1 className="text-xl font-bold">Feedbacks</h1>
@@ -19,11 +18,12 @@ export default function FeedbackManagePage() {
             {!isLoading && feedbacksData?.map((f) => {
                 const isAnon = f.visibility === 'Anonymous'
                 const isOpen = openId === f.id
+                const isRead = readIds.has(f.id)
 
                 return (
-                    <div key={f.id} className="border w-full rounded-lg shadow-sm p-4 bg-white">
+                    <div key={f.id} className={`border w-full rounded-lg shadow-sm p-4 ${isRead ? "bg-white" : "bg-yellow-50"}`}>
                         {/* Summary row */}
-                        <div  className="flex gap-2 justify-between items-center">
+                        <div onClick={() => handleToggle(f.id, isOpen)}  className="flex gap-2 justify-between items-center">
                             <div>
                                 {isAnon
                                     ? `${f.program.title} (${f.user.role})`
@@ -32,13 +32,12 @@ export default function FeedbackManagePage() {
                                 <span className="ml-2 text-sm text-gray-500">Date: {new Date(f.createdAt).toLocaleDateString()}</span>
                             </div>
 
-                            <button onClick={() => setOpenId(isOpen ? null : f.id)}  className="p-1 rounded-full hover:bg-gray-100 transition">
+                            <button  className="p-1 rounded-full hover:bg-gray-100 transition">
                                 {isOpen ? <IoClose size={18}/> : <FiPlus size={18}/> }
                              </button>
                         </div>
 
                         {/* Expanded details */}
-
                         {isOpen && (
                         <ul className="mt-3 space-y-1 text-sm text-gray-700">
                             <li>Visibility: {f.visibility}</li>
---
+++ src/hooks/feedback/useLocalStorageAdmin.ts
@@ -0,0 +1,49 @@
+import { useState, useEffect } from "react"
+
+export const useLocalStorageAdmin = () => {
+    const [openId, setOpenId] = useState<string | null>(null)
+    const [readIds, setReadIds] = useState<Set<string>>(() => {
+        if(typeof window !== "undefined") {
+            const stored = localStorage.getItem("readFeedbackIds")
+            if(stored) {
+                return new Set(JSON.parse(stored))
+            }
+        }
+        return new Set();
+    })
+
+    // Load localStorage para update
+        useEffect(() => {
+            const stored = localStorage.getItem("readFeedbackIds")
+            if(stored) {
+                try {
+                    const parsed: string[] = JSON.parse(stored)
+                    setReadIds(new Set(parsed))
+                } catch {
+                    console.error("Invalid localStorage data for readFeedbacks")
+                }
+            }
+        }, [])
+
+    // save localStorage if na open nayung feedbackId
+        useEffect(() => {
+            console.log("Saving to localStorage:", Array.from(readIds));
+            localStorage.setItem("readFeedbackIds", JSON.stringify(Array.from(readIds)))
+        }, [readIds])
+
+        const handleToggle = (id: string, isOpen: boolean) => {
+        if(isOpen) {
+            setOpenId(null)
+        } else {
+            setOpenId(id)
+            setReadIds(prev => {
+                const updated = new Set(prev)
+                updated.add(id)
+                return updated
+            })
+        }
+    }
+
+    return { openId, readIds, handleToggle }
+}
+
