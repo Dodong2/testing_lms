65a69b85cd922e56f93a995bea055fb4056a7953
Dodong2 <carlseighartaliode@gmail.com>
2025-08-22 17:25:55 +0800

    deployment test ver-8

--- src/app/(backend)/api/feedback/route.ts
+++ src/app/(backend)/api/feedback/route.ts
@@ -1,10 +1,10 @@
 import { prisma } from "@/lib/db";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 
 // create feedback by user
-export async function POST(req: NextResponse) {
+export async function POST(req: NextRequest) {
     try {
         const session = await getServerSession(authOptions)
         if(!session?.user?.email) {
