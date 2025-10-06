afc3ab5fa11cfab8ab3448391242619bf29297d0
Dodong2 <carlseighartaliode@gmail.com>
2025-10-06 21:21:48 +0800

    ver-9.5 added submission & grade in backend & frontend

--- src/app/(frontend)/(users)/home/participants/programs/[id]/[postId]/Submission/page.tsx
+++ src/app/(frontend)/(users)/home/participants/programs/[id]/[postId]/Submission/page.tsx
@@ -1,17 +1,16 @@
-
-interface SubmissionTask {
+interface SubmissionPageProps {
   params: {
     id: string
     postId: string
   }
 }
 
-export default function SubmissionPage ({ params }: SubmissionTask) {
+export default function SubmissionPage({ params }: SubmissionPageProps) {
   const { id: programId, postId } = params
+
   return (
     <div>
-       Welcome to submission page for program {programId}, post {postId}
+      Welcome to submission page for program {programId}, post {postId}
     </div>
   )
 }
-
