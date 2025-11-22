475480d932de2aa6bf3f7e7b9a1f3bae25829d40
Dodong2 <carlseighartaliode@gmail.com>
2025-11-22 15:51:59 +0800

    ver-12.5 adjust env

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,5 +1,6 @@
 /* step: 5 real-time
 Ito ang Socket.IO client ng frontend. Dito nakakonekta ang Next.js browser client sa Socket.IO server. */
+
 import { io } from "socket.io-client";
 
 const socketURL = process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:3001"
